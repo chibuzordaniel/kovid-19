914723afd2e85010234fdf9bc1291079be402f04
chibuzordaniel <starchrischibuzor@gmail.com>
2021-10-25 17:31:48 +1300

    update file

--- src/index.js/index.js
+++ src/index.js/index.js
@@ -1,2 +1,6 @@
-let date = document.getElementsByClassName('btn');
-console.log(date)
+// let date = document.getElementsByClassName('btn');
+// console.log(date)
+
+const d = new Date;
+console.log(d.toDateString())
+document.getElementById('demo').innerHTML = d.toDateString();
