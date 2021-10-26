fa2aceb0d8193b375899922cc106a63d871a96df
chibuzordaniel <starchrischibuzor@gmail.com>
2021-10-26 17:42:39 +1300

    update file

--- src/index.js/index.js
+++ src/index.js/index.js
@@ -4,3 +4,8 @@
 const d = new Date;
 console.log(d.toDateString())
 document.getElementById('demo').innerHTML = d.toDateString();
+
+
+const m = new Date;
+console.log(d.toLocaleDateString())
+document.getElementById('covid-date').innerHTML = m.toDateString();
