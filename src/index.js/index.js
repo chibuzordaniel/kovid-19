c62898dad28c8af24c44397d4e951b8eaf4928b7
chibuzordaniel <starchrischibuzor@gmail.com>
2021-11-09 16:35:41 +1300

    update file

--- src/index.js/index.js
+++ src/index.js/index.js
@@ -9,3 +9,20 @@ document.getElementById('demo').innerHTML = d.toDateString();
 const m = new Date;
 console.log(d.toLocaleDateString())
 document.getElementById('covid-date').innerHTML = m.toDateString();
+
+
+
+const url ="https://api.covid19api.com/summary";
+fetch(url)
+.then(res => res.json())
+.then(data =>{
+    console.log(data.Global.TotalConfirmed)
+    document.querySelector('.pom').innerHTML = data.Global.TotalConfirmed;
+    document.querySelector('.app').innerHTML = data.Global.NewConfirmed;
+    document.querySelector('.elo').innerHTML = data.Global.TotalRecovered;
+    document.querySelector('.kido').innerHTML = data.Global.TotalDeaths;
+}) 
+
+
+
+
